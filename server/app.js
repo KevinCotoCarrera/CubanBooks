5ea635623192dd7fd0a62dbe4c8d05720c47dfe6
Kevin <kevincoto2002@gmail.com>
2022-04-26 13:04:55 -0400

    added-admin-bro

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
+var session = require('express-session')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
 var app = express();
-
-
+const adminBro = new AdminBro ({
+  Databases: [],
+  rootPath: '/admin',
+})
+const router = AdminBroExpress.buildRouter (adminBro)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -24,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter)
+app.use(adminBro.options.rootPath, router)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
